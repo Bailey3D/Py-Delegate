595ddd6012ab2bb4a815980d50d609d29698d313
Bailey Martin <bhmbob@gmail.com>
2021-12-11 16:06:38 +0000

    [FIX] Previous commit submitted another repo?

--- setup.py
+++ setup.py
@@ -1,20 +1,28 @@
-import setuptools
+from setuptools import setup
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
 
-setuptools.setup(
-    name="delegate",
+def readme():
+    with open("README.rst") as f:
+        return f.read()
+
+
+setup(
+    name="minidelegate",
     version="0.0.1",
-    author="Bailey3D",
-    author_email="contact@bailey3d.com",
     description="Package for delegate based methods via decorators",
-    long_description=long_description,
-    url="https://github.com/BhMbOb/Py-Delegate",
-    packages=setuptools.find_packages(),
     classifiers=[
-        "Programming Language :: Python :: 3",
+        "Development Status :: 3 - Alpha",
         "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
     ],
+    keywords="developer delegate helpers decorators",
+    url="https://github.com/BhMbOb/Py-Delegate",
+    author="Bailey3D",
+    author_email="contact@bailey3d.com",
+    license="MIT",
+    packages=["delegate"],
+    install_requires=[],
+    include_package_data=True,
+    zip_safe=False
 )
